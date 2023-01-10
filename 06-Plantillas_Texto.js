7b2e7fbc86e89a2151919a89eed9bea01573fb00
Sabrina Contreras <sabriscon13@hotmail.com>
2023-01-10 15:20:33 -0700

    Tipos de texto

---
+++ 06-Plantillas_Texto.js
@@ -0,0 +1,14 @@
+//Plantilla de Texto
+
+const nombre_pasajero= "Leonardo";
+const apellido_pasajero= "LaCruz";
+
+let nombreCompleto=`El nombre completoes: ${nombre_pasajero}  ${apellido_pasajero}`;
+console.log(nombreCompleto);
+
+// Conversion de Texto
+
+let segundoPasajero="Diego Diaz";
+console.log(`El nombre del segundo Pasajero es ${segundoPasajero}`);
+segundoPasajero=132449;
+console.log(`El nombre del segundo Pasajero es ${segundoPasajero}`);
