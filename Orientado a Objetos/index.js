316a18a3dc53abc090746cc33ca2119535f9a011
Sabrina Contreras <sabriscon13@hotmail.com>
2023-01-15 16:37:50 -0700

    Creando lista de clientes OB

---
+++ Orientado a Objetos/index.js
@@ -0,0 +1,29 @@
+class Cliente
+{
+    nombreCliente;
+    dniCliente;
+    numeroCuenta;
+    saldoCuenta;
+}
+
+const cliente1=new Cliente()
+cliente1.nombreCliente="José";
+cliente1.dniCliente="134567";
+cliente1.numeroCuenta="12";
+cliente1.saldoCuenta=1000;
+console.log(cliente1);
+
+const cliente2=new Cliente()
+cliente2.nombreCliente="Eduardo";
+cliente2.dniCliente="138905";
+cliente2.numeroCuenta="45";
+cliente2.saldoCuenta=670;
+console.log(cliente2);
+
+const cliente3=new Cliente()
+cliente3.nombreCliente="Sara";
+cliente3.dniCliente="568923";
+cliente3.numeroCuenta="7";
+cliente3.saldoCuenta=896;
+
+console.log(cliente2);
