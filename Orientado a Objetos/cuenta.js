87202dfcc3139ea7f86c82721b808b72df66e8ef
Sabrina Contreras <sabriscon13@hotmail.com>
2023-01-24 21:00:10 -0700

    Crear cuenta de ahorro y normal

---
+++ Orientado a Objetos/cuenta.js
@@ -0,0 +1,34 @@
+export class cuenta{
+
+    #cliente;
+    #saldo;
+
+    constructor(cliente,saldo,numero,agencia){
+       this.numero=numero;
+       this.agencia=agencia; 
+       this.#cliente=cliente;
+       this.#saldo=saldo;
+    
+    }
+
+    depositoEnCuenta(valor) {
+        if (valor > 0)
+            this.#saldo += valor;
+        return this.#saldo;
+    }
+
+    retirarDeCuenta(valor) {
+        if (valor <= this.#saldo)
+            this.#saldo -= valor;
+        return this.#saldo;
+    }
+
+    verSaldo() {
+        return this.#saldo;
+    }
+
+    transferirParaCuenta(valor,cuentaDestino) {
+        this.retirarDeCuenta(valor);
+        cuentaDestino.depositoEnCuenta(valor);
+    }
+}
--- Orientado a Objetos/index.js
+++ Orientado a Objetos/index.js
@@ -1,38 +1,20 @@
 //Importación de clases
-import {CuentaCorriente} from "./cuentaCorriente.js";
 import {Cliente} from "./cliente.js";
+//import {cuentaAhorro} from "./cuentaAhorro.js";
+//import {CuentaCorriente} from "./cuentaCorriente.js";
+import {cuenta} from "./cuenta.js";
 
 
 // Definir clientes
 
 const cliente = new Cliente('Leonardo','13804050','123224');
 const cuentaDeLeonardo = new CuentaCorriente(cliente,'1','001');
-console.log(cuentaDeLeonardo.cliente);
+console.log(cuentaDeLeonardo);
 
 
 const cliente2 = new Cliente('Maria','16979808','8989');
 const cuentaDeMaria = new CuentaCorriente(cliente2,'2','002');
-console.log(cuentaDeMaria.cliente);
 
 
-cuentaDeLeonardo.depositoEnCuenta(250);
-var saldo = cuentaDeLeonardo.verSaldo(); 
-console.log('El Saldo actual (CuentaLeonardo) '+ saldo);
-
-/* Movimientos de cuenta saldo
-cuentaDeLeonardo.transferirParaCuenta(100,cuentaDeMaria);
-const saldoMaria = cuentaDeMaria.verSaldo();
-
-
-saldo = cuentaDeLeonardo.verSaldo();
-console.log('El saldo actual de María ' +saldoMaria);
-console.log('El saldo actual de Leonardo ' +saldo);
-
-if (cuentaDeMaria.cliente)
-    console.log(cuentaDeMaria.cliente);
-else
-    console.log(cuentaDeMaria);
-*/
-
-//Cantidad de cuentas por cliente
-console.log(CuentaCorriente.cantidadCuentas)
+const cuentaAhorroLeonardo=new cuentaAhorro(cliente,0,'987345','001');
+console.log(cuentaAhorroLeonardo);
